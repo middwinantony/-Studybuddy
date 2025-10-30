aa247da95d160cbe33e35fc84dd48ff5b73ad788
Pavel Mikhailau <paul.mikhailau@gmail.com>
2025-10-30 19:14:20 -0400

    Fixed conflict issue with Routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "topics#home"
-<<<<<<< HEAD
-=======
 
   resources :topics
   resources :chats, only: :show do
     resources :messages, only: :create
   end
->>>>>>> 7994d52cdf819687363dbe9253593df146c56e17
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
