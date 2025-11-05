4f168b12a6eca1d17abb3618fc066d38503f4a5d
Floriane Diem <florianediem@gmail.com>
2025-11-04 19:22:07 -0500

    chats history added

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,9 +1,15 @@
 class ChatsController < ApplicationController
+
+  def index
+    @topic = Topic.find(params[:id])
+    @chats = current_user.chats.where(challenge: @challenge)
+    @chat = Chat.new
+  end
+
   def show
     @chat = Chat.find(params[:id])
   end
 
   def new
-
   end
 end
