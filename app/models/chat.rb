1ee8c0c8e1bf4d43f24939a48178aee781d8d98a
middwinantony <middwin@gmail.com>
2025-11-08 17:27:19 -0500

    final done

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,7 +5,7 @@ class Chat < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
 
-  MAX_QUESTIONS = 5
+  MAX_QUESTIONS = 10
 
   TITLE_PROMPT = <<~PROMPT
     Generate a short, descriptive, 3-to-6-word title that summarizes the user question for a chat conversation.
